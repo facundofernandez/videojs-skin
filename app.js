c276ca828d937fd0234c8d5b72059fa4bea940f8
facundofernandez <ff.fernandez.facundo@gmail.com>
2017-10-29 16:09:35 -0300

    Angular full

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 "use strict";
 
+let configuraciones = {
+    general:{
+        border: false,
+        color: "red",
+        size: 0.5
+    },
+    botonPlay : {
+        radius: 100,
+        centrado: false,
+        size: 50,
+        height: 2,
+        width: 2,
+        bg: "red",
+        hoverBg: "brown",
+        shadow: false
+    },
+    controlBar : {
+        style:"1",
+        hide: false,
+        btnVelocidad: false,
+        size: 1,
+        bg: "red",
+        color: "#fff",
+        sliderColor: "#fff"
+    }
+}
 
-let player = {
-    theme: "default",
-    poster: 'https://www.anipedia.net/imagenes/imagenes-de-aves.jpg'
-};
-
-let themes = ["default", "youtube"];
-
-angular.module('tabsDemoDynamicHeight', ['ngMaterial']);
-angular.module('MyApp', ['ngMaterial', 'ngMessages'])
+angular.module('MyApp', ['ngMaterial','mdColorPicker'])
 
     .controller('playersCtrl', function ($scope) {
-        $scope.player = player;
-        $scope.themes = themes;
+
+        $scope.general       = configuraciones.general;
+        $scope.botonPlay     = configuraciones.botonPlay;
+        $scope.controlBar    = configuraciones.controlBar;
+        
+        $scope.themes = "";
+        
         $scope.update = function () {
-            let player = videojs('my_video_1');
-            player.removeClass('vjs-skin-yutube');
-            console.log(player);
-            player.addClass('vjs-skin-' + $scope.themeSelected);
-        };
-        $scope.themeSelected = '';
-        $scope.border = {
-            radius: 100,
-            centrado: false,
-            size: 50,
-            height: 2,
-            bg: "red",
-            hoverBg: "brown",
-            shadow: false
+
         };
 
-        $scope.controlBar = {
-            size: 1,
-            bg: "red",
-            color: "#fff"
-        };
-
-        $scope.updateBorder = function () {
+        $scope.updateCss = function () {
 
             let head = document.getElementsByTagName('head')[0];
             let style = head.querySelector("#estilos");
@@ -49,37 +53,17 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages'])
                 style.id = "estilos";
                 style.type = 'text/css';
                 head.appendChild(style);
-
-
             }
-
             style = head.querySelector("#estilos");
-            console.log($scope.border.centrado);
 
-            if($scope.border.shadow){
-                $scope.border.shadowCss = `
-                 box-shadow: -20px -10px 10px rgba(0, 0, 0, 0.3) inset, 10px 10px 10px rgba(0, 0, 0, 0.3);
-                 `;
-            }else{
-
-                $scope.border.shadowCss = "";
-            }
-
-            if($scope.border.centrado){
-                $scope.border.centradoCss = `/* Align center */
-                left: 50%;
-                top: 50%;
-                margin-left: -1em;
-                margin-top: -1em; `;
-            }else{
-
-                $scope.border.centradoCss = "";
-            }
-
-            let declarations = `
+            let css = `
             .vjs-skin-default {
-            
-              border: 0.5em solid #2B333F;
+              
+              ${$scope.general.border 
+                ? `border: ${$scope.general.size}em ${$scope.general.color} solid;` 
+                : ""
+              }
+
               font-size: 0.7em;
               color: #fff; }
               
@@ -94,81 +78,113 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages'])
                 
               .vjs-skin-default .vjs-menu-button .vjs-menu-content {
                 background-color: rgba(43, 51, 63, 0.7); }
+            
                 
-              .vjs-skin-default .vjs-progress-control .vjs-load-progress {
-                background-color: #cccccc; }
+                ${!$scope.controlBar.btnVelocidad ? `
+                    .vjs-skin-default .vjs-playback-rate {
+                    display:none; }
+                ` : "" }
                 
-              .vjs-skin-default .vjs-progress-control .vjs-slider {
-                background-color: #ababab; }
-                
-                .vjs-skin-default .vjs-progress-control .vjs-slider-bar {
-                  background-color: white; }
-                  
-              .vjs-skin-default .vjs-volume-level {
-                background: #fff; }
-                
-              .vjs-skin-default .vjs-big-play-button {
-                background-color: ${$scope.border.bg};
-                font-size: ${$scope.border.size}px;
-                line-height: ${$scope.border.height}em;
-                height: ${$scope.border.height}em;
-                width: ${$scope.border.height}em;
-                border: 0em solid #fff;
-                border-radius: ${$scope.border.radius}%;
-                
-                ${$scope.border.shadowCss}
-                ${$scope.border.centradoCss}
-                
+                .vjs-skin-default .vjs-play-progress, 
+                .vjs-skin-default .vjs-volume-level {
+                    background-color: ${$scope.controlBar.sliderColor};
                 }
+                
+                .vjs-skin-default .vjs-big-play-button {
+                    background-color: ${$scope.botonPlay.bg};
+                    font-size: ${$scope.botonPlay.size}px;
+                    line-height: ${$scope.botonPlay.height}em;
+                    height: ${$scope.botonPlay.height}em;
+                    width: ${$scope.botonPlay.width}em;
+                    border: 0em solid #fff;
+                    border-radius: ${$scope.botonPlay.radius}%;
+                    
+                    ${ $scope.botonPlay.shadow ? 
+                        `box-shadow: -20px -10px 10px rgba(0, 0, 0, 0.3) inset, 10px 10px 10px rgba(0, 0, 0, 0.3);` 
+                        : "" 
+                    }
+
+                    ${$scope.botonPlay.centrado ? `
+                        left: 50%;
+                        top: 50%;
+                        margin-left: -1em;
+                        margin-top: -1em;
+                        ` : ""
+                    }
+                }
+
+
+                ${$scope.controlBar.style === "1" ? `
+                    .vjs-skin-default .vjs-progress-control .vjs-progress-holder {
+                        margin: 0;
+                    }
+                    .vjs-skin-default .vjs-play-progress:before{
+                        content:"";
+                    }
+
+
+                    .vjs-skin-default .vjs-progress-control {
+                        position: absolute;
+                        transform: translate(0,-100%);
+                        width:100%;
+                        align-items: flex-end;
+                    }
+
+
+                    .vjs-skin-default .vjs-spacer,.video-js .vjs-time-control {
+                        display: flex;
+                        flex: 0 1 auto
+                    }
+
+                    .vjs-skin-default .vjs-time-control {
+                        padding:0 .5em;
+                    }
+
+                    .vjs-skin-default .vjs-spacer {
+                        flex: 1 1 auto
+                    }
+
+                    .vjs-skin-default .vjs-time-divider {
+                        padding:0;
+                        min-width:0;
+                    }
+                    
+                    .vjs-skin-default .vjs-remaining-time{
+                        display:none;
+                    }
+
+                ` : "" }
+
+
+                
                 
               .vjs-skin-default:hover .vjs-has-started .vjs-control-bar {
                 display: flex; }
                 
               .vjs-skin-default:hover .vjs-big-play-button {
 
-                background-color: ${$scope.border.hoverBg};
+                background-color: ${$scope.botonPlay.hoverBg};
                 color: rgba(255, 255, 255, 0.5); }
             
             div:not(.vjs-seeking):not(.vjs-waiting).video-js.vjs-paused.vjs-skin-default .vjs-big-play-button {
               display: block; }
+
+            ${$scope.controlBar.hide ? `
+              div:not(.vjs-seeking):not(.vjs-waiting).video-js.vjs-paused.vjs-skin-default .vjs-control-bar {
+                display: none; }
+              ` : "" 
+            }
             
-            div:not(.vjs-seeking):not(.vjs-waiting).video-js.vjs-paused.vjs-skin-default .vjs-control-bar {
-              display: none; }
             
             div:not(.vjs-seeking):not(.vjs-waiting).video-js.vjs-paused.vjs-skin-default .vjs-text-track-display {
               background: rgba(43, 51, 63, 0.5);
               height: 100%; }
-                        
-                        
-            
-            
             `;
 
-            console.log(style);
-            style.innerHTML = declarations;
+            style.innerHTML = css;
 
         };
-        $scope.updateBorder();
+        $scope.updateCss();
 
-    })
-
-
-    .directive('videoJs', function () {
-        return {
-            restrict: 'E',
-            controller: 'playersCtrl',
-            templateUrl: 'components/video/video.html',
-            link: function (scope, element, attr) {
-                //console.log(scope, element, attr);
-                //let btnPlay = element[0].getElementsByClassName('vjs-big-play-button');
-                //console.log(btnPlay[0].style);
-                let btnPlay = document.querySelector('.vjs-big-play-button');
-                console.log(btnPlay);
-                /*btnPlay[0].css({
-                    "border-radius": scope.border.radius + "%"
-                });
-                */
-            }
-        };
     });
 
